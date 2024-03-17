2a5448872557b42c44138f719a97a9be249e187e
FosterSOAsare <fostersoasare@gmail.com>
2024-03-17 12:43:36 +0000

    Roadmap responsiveness

--- src/components/Roadmap.tsx
+++ src/components/Roadmap.tsx
@@ -2,10 +2,11 @@ import React, { useState } from "react";
 
 import roadmap from "@/data/roadmap";
 
-import { MdOutlineKeyboardArrowUp, MdOutlineKeyboardArrowDown } from "react-icons/md";
+import { MdOutlineKeyboardArrowUp, MdOutlineKeyboardArrowDown, MdOutlineKeyboardArrowLeft, MdOutlineKeyboardArrowRight } from "react-icons/md";
 
 const Roadmap = () => {
 	const [active, setActive] = useState(1);
+	const [mobileSlider, setMobileSlider] = useState(1);
 	return (
 		<section className="w-full py-24" id="roadmap">
 			<h3 className="text-3xl text-center">Project Roadmap</h3>
@@ -14,7 +15,8 @@ const Roadmap = () => {
 					<h3 className="font-bold text-2xl mt-6 md:mt-0 md:text-3xl lg:text-6xl md:-rotate-90"> 2024</h3>
 				</aside>
 				<div>
-					<div className="w-full h-[100vh] bg-[red] overflow-hidden relative mt-8">
+					{/* For desktop */}
+					<div className="hidden md:block w-full h-[100vh] overflow-hidden relative mt-8">
 						<div className={`relative ${active === 1 ? "top-0" : "-top-[32%]"} transition-all duration-700`}>
 							{roadmap?.map((item, index) => (
 								<div className="flex items-stretch justify-center" key={index}>
@@ -42,6 +44,26 @@ const Roadmap = () => {
 							))}
 						</div>
 					</div>
+					{/* For mobile */}
+					<div className="w-[100vw] overflow-hidden relative mt-8">
+						<div
+							className={`relative transition-all duration-700 flex items-stretch justify-between`}
+							style={{ width: `${roadmap?.length * 100}%`, marginLeft: `-${(mobileSlider - 1) * 100}%` }}>
+							{roadmap?.map((item, index) => (
+								<div className="w-full p-4" key={index}>
+									<h3 className="opacity-50 font-medium mb-2">{item?.time}</h3>
+									<h1 className="text-2xl text-white font-bold mb-6">Phase {index + 1}</h1>
+									<ul className="list-disc w-[90%] ml-5 pb-10">
+										{item?.features?.map((feature, index) => (
+											<li className="w-full text-[18px] mb-[5px]" key={index}>
+												{feature}
+											</li>
+										))}
+									</ul>
+								</div>
+							))}
+						</div>
+					</div>
 				</div>
 				{/* For desktop */}
 				<div className="hidden md:block w-1/5">
@@ -58,16 +80,18 @@ const Roadmap = () => {
 				</div>
 
 				{/* For mobile  */}
-				<div className="1/5 flex items-center justify-center">
+				<div className="md:hidden mt-6 flex items-center gap-4 justify-center">
 					<button
-						className={`w-12 h-12 border-[1px] border-blue text-blue  ${active === 2 ? "opacity-100 " : "opacity-20"} flex items-center justify-center mb-2 rounded-full`}
-						onClick={() => setActive(1)}>
-						<MdOutlineKeyboardArrowUp className={`text-3xl`} />
+						disabled={mobileSlider == 0}
+						className={`w-12 h-12 border-[1px] border-blue text-blue  ${mobileSlider > 1 ? "opacity-100 " : "opacity-20"} flex items-center justify-center mb-2 rounded-full`}
+						onClick={() => setMobileSlider((prev) => --prev)}>
+						<MdOutlineKeyboardArrowLeft className={`text-3xl`} />
 					</button>
 					<button
-						className={`w-12 h-12 border-[1px] border-blue text-blue ${active === 1 ? "opacity-100 " : "opacity-20"} flex items-center justify-center mb-2 rounded-full`}
-						onClick={() => setActive(2)}>
-						<MdOutlineKeyboardArrowDown className={`text-3xl`} />
+						disabled={mobileSlider == roadmap?.length}
+						className={`w-12 h-12 border-[1px] border-blue text-blue ${mobileSlider < roadmap.length ? "opacity-100 " : "opacity-20"} flex items-center justify-center mb-2 rounded-full`}
+						onClick={() => setMobileSlider((prev) => ++prev)}>
+						<MdOutlineKeyboardArrowRight className={`text-3xl`} />
 					</button>
 				</div>
 			</div>
